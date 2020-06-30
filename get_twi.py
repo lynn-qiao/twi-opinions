87068f0eab1587fe21c35ac9c92e6278bf2aa789
lynn-qiao <63971654+lynn-qiao@users.noreply.github.com>
2020-06-30 13:55:33 -0400

    m

--- get_twi.py
+++ get_twi.py
@@ -20,17 +20,17 @@ Created on Mon Jun 29 22:47:51 2020
 
 import pandas
 
-us_state=["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
-          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
-          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
-          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
-          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
 
 #testfile
 testfile=pandas.read_csv('testfile.csv')
 
 def filter_non_us(testfile):
-    
+    us_state=["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
+          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
+          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
     df=testfile[['text','place']]
     #fill na with na-marker
     df=df.fillna(value='ZZZ')
--- sentimentpy.py
+++ sentimentpy.py
@@ -5,6 +5,17 @@
 #try textbolb
 #note that textblob is installed under python3.8.3,
 # should update it into conda 
+#textblob only has polarity????
+#okey textblob need machine learning to categorize emotion
+#
 
 from textblob import TextBlob
+import get_twi
 
+import pandas
+from emotion_predictor import EmotionPredictor
+
+#testfile
+testfile=pandas.read_csv('testfile.csv')
+
+filtered=get_twi.filter_non_us(testfile)
